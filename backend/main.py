07603b5cd6b895d04cd62b6bdc6c2c6fb2509f59
vimsd <cheertahh@gmail.com>
2026-01-23 21:48:25 +0700

    Comment

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, ForeignKey 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from flask_migrate import Migrate
 
@@ -23,14 +23,32 @@ class TodoItem(db.Model):
     title: Mapped[str] = mapped_column(String(100))
     done: Mapped[bool] = mapped_column(default=False)
 
+    comments: Mapped[list["Comment"]] = relationship(back_populates="todo")
+
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "done": self.done
+            "done": self.done,
+            "comments": [
+                comment.to_dict() for comment in self.comments
+            ]
         }
-    def new_todo(data):
-        return TodoItem(title=data['title'], done=data.get('done', False))
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    message: Mapped[str] = mapped_column(String(250))
+    todo_id: Mapped[int] = mapped_column(ForeignKey('todo_item.id'))
+
+    todo: Mapped["TodoItem"] = relationship(back_populates="comments")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "todo_id": self.todo_id
+        }
+
 
 INITIAL_TODOS = [
     TodoItem(title='Learn Flask'),
